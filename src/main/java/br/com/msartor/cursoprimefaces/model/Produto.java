f650c0492d9a02d3236140b01e0e172467e41388
marcelo <marcelo_sartor@hotmail.com>
2016-03-14 17:50:11 -0300

    classes até a aula 10.5

---
+++ src/main/java/br/com/msartor/cursoprimefaces/model/Produto.java
@@ -0,0 +1,79 @@
+package br.com.msartor.cursoprimefaces.model;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+public class Produto implements Serializable {
+
+	
+	private static final long serialVersionUID = 1L;
+	private Long id;
+	private String nome;
+	private BigDecimal valorUnitario;
+	private Integer quantidadeEstoque;
+	private Categoria categoria;
+
+	/*
+	 * Get and Set
+	 */
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public BigDecimal getValorUnitario() {
+		return valorUnitario;
+	}
+	public void setValorUnitario(BigDecimal valorUnitario) {
+		this.valorUnitario = valorUnitario;
+	}
+	public Integer getQuantidadeEstoque() {
+		return quantidadeEstoque;
+	}
+	public void setQuantidadeEstoque(Integer quantidadeEstoque) {
+		this.quantidadeEstoque = quantidadeEstoque;
+	}
+	public Long getId() {
+		return id;
+	}
+	
+	public Categoria getCategoria() {
+		return categoria;
+	}
+	
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+	
+	/*
+	 * hashCode and equals
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Produto other = (Produto) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
+	
+
+	
+}
---
+++ src/main/java/br/com/msartor/cursoprimefaces/model/TipoPessoa.java
@@ -0,0 +1,5 @@
+package br.com.msartor.cursoprimefaces.model;
+
+public enum TipoPessoa {
+	FISICA,JURIDICA;
+}
