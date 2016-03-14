2837cbde1bf4bb4a550f762229db2bd7bf9c2040
marcelo <marcelo_sartor@hotmail.com>
2016-03-14 15:23:11 -0300

    inicia ambiente

---
+++ src/main/webapp/resources/alvorada/javascripts/app.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+	$('.js-toggle').bind('click',function(){
+		$('.js-sidebar').toggleClass('is-toggle');
+		$('.js-content').toggleClass('is-toggle');
+	})
+});
+
+
+
+
+	
